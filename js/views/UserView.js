e7440c561303d9524806c24f7f3a73019157406f
TiagoNandes <tiagonandes@hotmail.com>
2020-05-27 11:23:22 +0100

    assets fix

--- js/views/UserView.js
+++ js/views/UserView.js
@@ -5,13 +5,13 @@ export default class UserView {
         this.userController = new UserController();
 
         // register DOM
-        this.registerForm = document.getElementById('frmRegister');
+        //this.registerForm = document.getElementById('frmRegister');
         this.registerUsername = document.getElementById('txtUsernameRegister');
         this.registerPassword = document.getElementById('txtPasswordRegister');
         this.registerPassword2 = document.getElementById('txtPasswordRegister2');
         this.registerMessage = document.getElementById('mdlRegisterMessage');
 
-        this.bindAddRegisterForm();
+        //this.bindAddRegisterForm();
 
         // login DOM
         this.loginForm = document.getElementById('frmLogin');
@@ -24,30 +24,30 @@ export default class UserView {
         // buttons DOM
         this.loginButton = document.getElementById('btnLogin');
         this.registerButton = document.getElementById('btnRegister');
-        this.logoutButton = document.getElementById('btnLogout');
+        //this.logoutButton = document.getElementById('btnLogout');
 
-        this.bindAddLogoutEvent();
+        //this.bindAddLogoutEvent();
 
-        this.checkLoginStatus();     
+        //this.checkLoginStatus();     
     }
 
-    bindAddRegisterForm() {
-        this.registerForm.addEventListener('submit', event => {
-            event.preventDefault();
+    // bindAddRegisterForm() {
+    //     this.registerForm.addEventListener('submit', event => {
+    //         event.preventDefault();
 
-            try {
-                if (this.registerPassword.value !==this.registerPassword2.value) {
-                    throw Error('Password and Confirm Password are not equal');   
-                }
-                this.userController.createUser(this.registerUsername.value, this.registerPassword.value);
-                this.displayRegisterMessage('User registered with success!', 'success');
-                alert("Sucesso!")
-                location.href='login.html';
-            } catch(e) {
-                this.displayRegisterMessage(e, 'danger');
-            }
-        });
-    }
+    //         try {
+    //             if (this.registerPassword.value !==this.registerPassword2.value) {
+    //                 throw Error('Password and Confirm Password are not equal');   
+    //             }
+    //             this.userController.createUser(this.registerUsername.value, this.registerPassword.value);
+    //             this.displayRegisterMessage('User registered with success!', 'success');
+    //             alert("Sucesso!")
+    //             location.href='login.html';
+    //         } catch(e) {
+    //             this.displayRegisterMessage(e, 'danger');
+    //         }
+    //     });
+    // }
 
     bindAddLoginForm() {
         this.loginForm.addEventListener('submit', event => {
@@ -72,21 +72,21 @@ export default class UserView {
         });
     }
     
-    bindAddLogoutEvent() {
-        this.logoutButton.addEventListener('click', event => {
-            this.userController.logoutUser();
-            this.updateButtons('logout');
-            location.reload()
-        });
-    }
+    // bindAddLogoutEvent() {
+    //     this.logoutButton.addEventListener('click', event => {
+    //         this.userController.logoutUser();
+    //         this.updateButtons('logout');
+    //         location.reload()
+    //     });
+    // }
 
-    checkLoginStatus() {
-        if (this.userController.checkLoginStatus()) {
-            this.updateButtons('login');
-        } else {
-            this.updateButtons('logout');
-        }
-    }
+    // checkLoginStatus() {
+    //     if (this.userController.checkLoginStatus()) {
+    //         this.updateButtons('login');
+    //     } else {
+    //         this.updateButtons('logout');
+    //     }
+    // }
 
     displayRegisterMessage(message, type) {
         this.registerMessage.innerHTML =
@@ -98,17 +98,17 @@ export default class UserView {
             `<div class="alert alert-${type}" role="alert">${message}</div>`;
     }
 
-    updateButtons(event) {
-        switch(event) {
-            case 'login':
-                this.loginButton.style.visibility = 'hidden'
-                this.registerButton.style.visibility = 'hidden'
-                this.logoutButton.style.visibility = 'visible'
-                break;
-            case 'logout':
-                this.loginButton.style.visibility = 'visible'
-                this.registerButton.style.visibility = 'visible'
-                this.logoutButton.style.visibility = 'hidden'
-        }
-    }
+    // updateButtons(event) {
+    //     switch(event) {
+    //         case 'login':
+    //             this.loginButton.style.visibility = 'hidden'
+    //             this.registerButton.style.visibility = 'hidden'
+    //             this.logoutButton.style.visibility = 'visible'
+    //             break;
+    //         case 'logout':
+    //             this.loginButton.style.visibility = 'visible'
+    //             this.registerButton.style.visibility = 'visible'
+    //             this.logoutButton.style.visibility = 'hidden'
+    //     }
+    // }
 }
